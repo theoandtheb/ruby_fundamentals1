15e5f49d3f0770fc898a57dcc5b7c386ae402e42
theoandtheb <blue.apple@sympatico.ca>
2014-06-03 14:08:18 -0400

    Added the torturous confusulation rb.

---
+++ exercise2_confusulation.rb
@@ -0,0 +1,26 @@
+sleep 0.8
+print "\n\nAs you may have guessed, I like pipes."
+sleep 1.2
+print "  I also happen to like ampersands."
+sleep rand
+print "."
+sleep rand
+print "."
+sleep rand
+print "  A lot."
+sleep 1.5
+print "\n\nTo be honest,"
+sleep 0.6
+print "  I really don\'t care for expressions."
+sleep 0.8
+print "  Some might say that I like to torture them."
+sleep 1.2
+print "\n\nIf I'm really bored, I like to ask Ruby to work out the seamingly impossible."
+sleep 1.3
+print "\n\nIn fact, I'm going to give it a try right now."
+sleep 0.8
+print "\n\n\"Hey Ruby! (true && false) || (false && true) || !(false && false).\""
+sleep 1.4
+print "\n\nTrue or false?\n"
+puts (true && false) || (false && true) || !(false && false)
+print "\n"
